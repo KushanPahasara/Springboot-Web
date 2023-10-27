582c4c55563835a09e1bedfc20980c92796c7b38
KushanPahasara <kushanpahasaradharmawansha@gmail.com>
2023-10-27 18:49:26 +0530

    Add navbar and footer

---
+++ FrontEnd/react-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class HeaderComponent extends Component {
+  render() {
+    return (
+      <div>
+           <header className='header'>
+
+           <nav class="navbar navbar-dark bg-primary">
+            <div><a href='' className='navbar-brand'>Item Management Web</a></div>
+            </nav>
+
+            
+           </header>
+          
+      </div>
+    )
+  }
+}
