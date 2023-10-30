2ee3525f4d82ead8bed35c99441560e4197f29ff
KushanPahasara <kushanpahasaradharmawansha@gmail.com>
2023-10-30 11:30:15 +0530

    Add routes

--- FrontEnd/react-frontend/src/App.js
+++ FrontEnd/react-frontend/src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <HeaderComponent/>
+    <Router>
+    
+      <HeaderComponent />
       <div className="container">
-       <ListEmployeeComponent/>
-     </div>
-      <FooterComponent/>
-    </div>
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />} />
+          <Route path="/employees" element={<ListEmployeeComponent />} />
+        </Routes>
+      </div>
+      <FooterComponent />
+    
+  </Router>
   );
 }
+
 
 export default App;
